d8e8ea4dacd6a3a4841c914c94bacb60ea2a1de3
salma maayez <maizesalma13@gmail.com>
2024-09-15 00:24:13 +0100

    autre com

--- src/_mock/account.js
+++ src/_mock/account.js
@@ -1,7 +1,6 @@
 // ----------------------------------------------------------------------
 
 export const account = {
-  displayName: 'salma maize',
-  email: 'maizesalma13@gmail.com',
-  photoURL: '/assets/images/avatars/avatar_1.jpg',
+  displayName: 'Chawki Gharsellaoui',
+  photoURL: '/assets/images/avatars/avatar_12.jpg',
 };
